f6c53845cdc2f95bac7f391e1d6f8475b4602244
rishita.hariyani@gmail.com <rishita.hariyani@gmail.com>
2019-01-20 21:14:49 -0700

    final

---
+++ Task6/Pseudocode.cs
@@ -0,0 +1,31 @@
+////set up
+
+//note: all in green commnets because program show run time error due to this file
+
+double ordertotal = 0.0;
+double totaltax = 0.0;
+double grandtotal = 0.0;
+double statetax = 0.0;
+double salestax = 0.0;
+double tax1 = 0.03;
+double tax2 = 0.05;
+
+////input
+
+//    "What is your odertotal?";
+
+
+//    //process
+
+//    salestax=ordertotal*tax1 
+//put "The salestax is:"+Salestax
+//    statetax=ordertotal*tax2       
+//put "The statetax is:"+statetax
+
+//    totaltax amt = salestax + statetax;
+//put "The total tax amt is:"+taotal tax
+//    grandtotal=totaltax+ordertotal;
+//put "The grandtotal is:"+grandtotal
+//    
+
+
