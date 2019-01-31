a0eaf048587fa27c4ea0dc504d264de231d80648
rishita.hariyani@gmail.com <rishita.hariyani@gmail.com>
2019-01-30 20:11:19 -0700

    redone with function

--- Task6/Task6/Program.cs
+++ Task6/Task6/Program.cs
@@ -5,46 +5,46 @@ namespace Task6
     class Program
     {
         static void Main(string[] args)
+
         {
             double ordertotal = 0.0;
-            double totaltax = 0.0;
+            //double totaltax = 0.0;
             double grandtotal = 0.0;
-            double statetax = 0.0;
-            double salestax = 0.0;
-            
-            double tax1= 0.03;
-            double tax2= 0.05;
+            double statetax = 0.05;
+            double salestax = 0.03;
+            string question = "Y";
+
+            do
+            {
 
 
-            Console.WriteLine("What is your ordertotal? ");
-            ordertotal = double.Parse(Console.ReadLine());
+                Console.WriteLine("What is your ordertotal? ");
+                ordertotal = double.Parse(Console.ReadLine());
 
+                grandtotal = Taxcal(ordertotal, ref statetax, ref salestax);
+                Console.WriteLine("Would you like to do another order (Y/N)");
+                question = Console.ReadLine();
 
-            
-                salestax = ordertotal * tax1; 
-                Console.WriteLine("The sales tax on your oder is:" + salestax);
-                Console.ReadLine();
-            
-                statetax = ordertotal * tax2;
-                Console.WriteLine("The state tax on your oder is:" + statetax);
-                Console.ReadLine();
+            } while (question == "Y");
 
-
-                  totaltax = salestax + statetax;
-                Console.WriteLine("The total tax on your oder is:" + totaltax);
-                Console.ReadLine();
-
-
-
-                 grandtotal = totaltax + ordertotal;
-                Console.WriteLine("The grandtotal on your oder is:" + grandtotal);
-                Console.ReadLine();
-
-           
         }
+        private static double Taxcal(double ordertotal, ref double statetax, ref double salestax)
+        {
+            double grandtotal;
+            salestax = ordertotal * salestax;
+            statetax = ordertotal * statetax;
+            grandtotal = statetax + ordertotal + salestax;
+
+            Console.WriteLine("The sales tax on your oder is:" + salestax);
+            Console.WriteLine("The state tax on your oder is:" + statetax);
+            Console.WriteLine("The grandtotal on your oder is:" + grandtotal);
+            Console.ReadLine();
+            return grandtotal;
+        }
+
     }
-    }
-    
+
+}
 
 
 
